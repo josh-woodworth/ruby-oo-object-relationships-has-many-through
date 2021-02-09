8a5661638051feb537d70b9089881b33abbd438d
Josh Woodworth <72992058+josh-woodworth@users.noreply.github.com>
2021-02-08 23:29:55 -0700

    commit tree 6fc37d2cc2a4ff7a0df79b6aaa49ddfa2a34f68e

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
 class Waiter
 
+    attr_accessor :name, :experience
+    @@all = []
+
+    def initialize(name, experience)
+        @name = name
+        @experience = experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        best_tip = meals.max do |meal_1, meal_2|
+            meal_1.tip <=> meal_2.tip
+        end
+        best_tip.customer
+    end
+
 end
